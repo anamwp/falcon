34e33ea9b4aa5263c6f57b51f1e74977c8138eeb
anamwp <theanamhossain@gmail.com>
2025-03-06 15:16:44 +0600

    [delete] devsage rename

--- themes/devsage/resources/views/partials/content-page.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="page-content pb-15">
-  @php(the_content())
-</div>
-@if ($pagination)
-  <nav class="page-nav" aria-label="Page">
-    {!! $pagination !!}
-  </nav>
-@endif
--- themes/devsage/resources/views/partials/page-header.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="page-header my-10">
-  <h1 class="text-2xl">{!! $title !!}</h1>
-</div>
